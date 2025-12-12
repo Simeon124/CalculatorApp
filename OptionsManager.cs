af48616ef08ecba4512fe3c7b704434c5213a026
XII class <viktor010607@gmail.com>
2025-12-12 09:54:54 +0200

    mazna

---
+++ OptionsManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply"
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+        public static void Power(double a, double b)
+        {
+            Console.WriteLine($"{a}^{b} = {Math.Pow(a, b)}");
+        }
+        public static void Log(double a, double b)
+        {
+            Console.WriteLine($"Log of {a} with base {b} = {Math.Log(a, b)}");
+        }
+        public static void Factorial(double a, double b)
+        {
+            long factA = CalculateFact((int)a);
+            long factB = CalculateFact((int)b);
+            Console.WriteLine($"{a}! + {b}! = {(int)a}! + {(int)b}! = {factA + factB}");
+        }
+        private static long CalculateFact(int a)
+        {
+            long result = a;
+            for (int i = a - 1; i >= 1; i--)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+    }
+}
